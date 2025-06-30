8123d011cea1531705d6b1ba848f8912779b79f4
Kassandra <kam.terbeek@gmail.com>
2025-06-30 03:06:11 -0600

    Update event.js

--- event.js
+++ event.js
@@ -1,1 +1,14 @@
+msg.alert = false;
 
+if (msg.payload.hasOwnProperty("temperature")) {
+  if (msg.payload.temperature <= 0) {
+    msg.alert = true;
+    msg.alertType = "LOW_TEMP";
+    msg.alertDescription = `Low temperature alert (${msg.payload.temperature} for Boulder Garden)`
+  } else if (msg.payload.temperature >= 27) {
+    msg.alert = true;
+    msg.alertType = "HIGH_TEMP";
+    msg.alertDescription = `High temperature alert (${msg.payload.temperature} for Boulder Garden)`
+  }
+}
+return msg;
